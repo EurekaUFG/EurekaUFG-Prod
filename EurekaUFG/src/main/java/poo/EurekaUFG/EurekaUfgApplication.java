e6b6a8ee3de7053858d6b497210710907348ea27
Diogo Devotte Ferri <diogodevotteferri07@gmail.com>
2025-11-12 18:43:06 -0300

    Nova branch como Initializr

---
+++ EurekaUFG/src/main/java/poo/EurekaUFG/EurekaUfgApplication.java
@@ -0,0 +1,13 @@
+package poo.EurekaUFG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaUfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaUfgApplication.class, args);
+	}
+
+}
---
+++ EurekaUFG/src/test/java/poo/EurekaUFG/EurekaUfgApplicationTests.java
@@ -0,0 +1,13 @@
+package poo.EurekaUFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaUfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
