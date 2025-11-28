5711cde30e9f00dd6cc505c962e5d9b0ee9f42c0
Diogo Devotte Ferri <diogodevotteferri07@gmail.com>
2025-11-28 09:07:35 -0300

    ItemService

--- src/main/java/poo/EurekaUFG/controller/ItemController.java
+++ src/main/java/poo/EurekaUFG/controller/ItemController.java
@@ -5,20 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import poo.EurekaUFG.model.dto.ItemRequestDTO;
-import poo.EurekaUFG.model.dto.ItemResponseDTO;
 import poo.EurekaUFG.model.entity.Item;
-import poo.EurekaUFG.model.entity.LocalDeixou;
-import poo.EurekaUFG.repositories.ItemRepository;
+import poo.EurekaUFG.service.ItemService;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/itens")
@@ -26,11 +17,11 @@ import java.util.UUID;
 public class ItemController {
 
     @Autowired
-    private ItemRepository itemRepository;
+    private ItemService itemService;
 
     @GetMapping
     public List<Item> getAll() {
-        return itemRepository.findAll();
+        return itemService.findAll();
     }
 
     @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
@@ -44,55 +35,17 @@ public class ItemController {
             @RequestParam("imagem") MultipartFile imagemFile
     ) throws IOException {
 
-        if (imagemFile == null || imagemFile.isEmpty()) {
-            return ResponseEntity.badRequest().body("Imagem é obrigatória");
-        }
-
-        String uploadDir = "uploads/";
-        File dir = new File(uploadDir);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-
-        String fileName = UUID.randomUUID() + "_" + imagemFile.getOriginalFilename(); // CRIA UM NOME ÚNICO PARA O ARQUIVO DA IMAGEM
-        Path filePath = Paths.get(uploadDir, fileName);
-        Files.copy(imagemFile.getInputStream(), filePath);
-
-        LocalDeixou localDeixouEnum = LocalDeixou.valueOf(localDeixou);
-        LocalDate dataConvertida = LocalDate.parse(data);
-
-        ItemRequestDTO dto = new ItemRequestDTO(
-                nome,
-                descricao,
-                localAchou,
-                localDeixouEnum,
-                dataConvertida,
-                matriculaAchou,
-                imagemFile
-        );
-
-        // Criar a entidade usando o construtor existente
-        Item item = new Item(dto, filePath.toString());
-
-        itemRepository.save(item);
+        itemService.createItem(nome, descricao, localAchou, localDeixou, data, matriculaAchou, imagemFile);
 
         return ResponseEntity.ok("Item cadastrado com sucesso!");
     }
 
+
     @PutMapping("/{id}")
-    public ResponseEntity<Item> atualizarItem(@PathVariable Long id, @RequestBody Item itemAtualizado) {
-        Item item = itemRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Item não encontrado"));
-
-        item.setNome(itemAtualizado.getNome());
-        item.setDescricao(itemAtualizado.getDescricao());
-        item.setLocalAchou(itemAtualizado.getLocalAchou());
-        item.setLocalDeixou(itemAtualizado.getLocalDeixou());
-        item.setStatusItem(itemAtualizado.getStatusItem());
-        item.setMatriculaPerdeu(itemAtualizado.getMatriculaPerdeu());
-        // se quiser permitir alterar outros campos, adicione aqui
-
-        Item salvo = itemRepository.save(item);
+    public ResponseEntity<Item> atualizarItem(
+            @PathVariable Long id,
+            @RequestBody Item itemAtualizado) {
+        Item salvo = itemService.atualizarItem(id, itemAtualizado);
         return ResponseEntity.ok(salvo);
     }
 
--- src/main/java/poo/EurekaUFG/service/ItemService.java
+++ src/main/java/poo/EurekaUFG/service/ItemService.java
@@ -1,20 +1,114 @@
-// package poo.EurekaUFG.service;
+package poo.EurekaUFG.service;
 
-// import poo.EurekaUFG.repository.ItemRepository;
-// import poo.EurekaUFG.model.entity.Item;
-// import poo.EurekaUFG.model.entity.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import poo.EurekaUFG.model.dto.ItemRequestDTO;
+import poo.EurekaUFG.model.entity.Item;
+import poo.EurekaUFG.model.entity.LocalDeixou;
+import poo.EurekaUFG.repositories.ItemRepository;
 
-// import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
-// public class ItemService {
+@Service
+public class ItemService {
 
-//     private final ItemRepository dao = new ItemRepository();
+    @Autowired
+    private ItemRepository itemRepository;
 
-//     public List<Item> listar() throws Exception {
-//         return dao.listar();
-//     }
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
 
-//     public void criar(Item item, Usuario usuario_achou) throws Exception{
-//         dao.salvar(item,usuario_achou);
-//     }
-// }
+
+    public void createItem(
+            String nome,
+            String descricao,
+            String localAchou,
+            String localDeixou,
+            String data,
+            String matriculaAchou,
+            MultipartFile imagemFile
+    ) throws IOException {
+
+        if (imagemFile == null || imagemFile.isEmpty()) {
+            throw new IllegalArgumentException("Imagem é obrigatória");
+        }
+
+        // Criar pasta caso não exista
+        String uploadDir = "uploads/";
+        File dir = new File(uploadDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        // Gerar nome único
+        String fileName = UUID.randomUUID() + "_" + imagemFile.getOriginalFilename();
+        Path filePath = Paths.get(uploadDir, fileName);
+
+        Files.copy(imagemFile.getInputStream(), filePath);
+
+        // Converter valores
+        LocalDeixou localDeixouEnum = LocalDeixou.valueOf(localDeixou);
+        LocalDate dataConvertida = LocalDate.parse(data);
+
+        // Criar DTO
+        ItemRequestDTO dto = new ItemRequestDTO(
+                nome,
+                descricao,
+                localAchou,
+                localDeixouEnum,
+                dataConvertida,
+                matriculaAchou,
+                imagemFile
+        );
+
+        // Criar entidade
+        Item item = new Item(dto, filePath.toString());
+
+        // Salvar no banco
+        itemRepository.save(item);
+    }
+
+    public Item atualizarItem(Long id, Item  itemAtualizado) {
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item não encontrado"));
+
+        item.setNome(itemAtualizado.getNome());
+        item.setDescricao(itemAtualizado.getDescricao());
+        item.setLocalAchou(itemAtualizado.getLocalAchou());
+        item.setLocalDeixou(itemAtualizado.getLocalDeixou());
+        item.setStatusItem(itemAtualizado.getStatusItem());
+        item.setMatriculaPerdeu(itemAtualizado.getMatriculaPerdeu());
+        // se quiser permitir alterar outros campos, adicione aqui
+
+        return itemRepository.save(item);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
